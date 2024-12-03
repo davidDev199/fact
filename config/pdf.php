7bf85504fa3cb6c689ee8ee410551d5b72789004
DavidDargui <daviddevyauri@gmail.com>
2024-12-03 11:18:07 -0500

    fix

--- config/pdf.php
+++ config/pdf.php
@@ -3,5 +3,5 @@
 return [
     'wkhtmltopdf_bin' => strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'
         ? env('WKHTMLTOPDF_BINARIES_WINDOWS', 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
-        : env('WKHTMLTOPDF_BINARIES_LINUX', '/usr/local/bin/wkhtmltopdf'),
+        : env('WKHTMLTOPDF_BINARIES_LINUX', '/usr/bin/wkhtmltopdf'),
 ];
